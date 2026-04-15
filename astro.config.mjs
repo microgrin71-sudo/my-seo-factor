ec57547b2e62acf7d9acf4661090c1965d2a8ead
microgrin71-sudo <micro.grin71@gmail.com>
2026-04-15 15:16:52 +0300

    [SEO-Module] Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,4 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  content: {
-    collections: {
-      blog: {
-        schema: ({ z }) => ({
-          title: z.string(),
-          description: z.string().optional(),
-          date: z.string().optional(),
-          keywords: z.array(z.string()).optional(),
-        }),
-      },
-    },
-  },
 });
